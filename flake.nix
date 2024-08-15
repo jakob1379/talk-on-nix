424c135869c3faf6fe5da75a9d1b09e17358786d
Jakob Guldberg Aaes <jakob.aaes@res-group.com>
2024-08-15 17:23:44 +0200

    migrated to revealjs

--- flake.nix
+++ flake.nix
@@ -12,17 +12,33 @@
       generalPackages = pkgs: with pkgs; [
         reveal-md
         marksman
+        nodejs
       ];
 
     in {
+      # make sure that 
+      
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
+
+          
           packages = (generalPackages pkgs); # ++ (pythonBasePackages pkgs);
 
           shellHook = ''
-          echo "Welcome to the ❄️nix❄️ shell!"
-          '';
+            echo "Welcome to the ❄️nix❄️ shell!"
+            if [ ! -d reveal.js ]; then
+              echo "Cloning reveal.js repository..."
+              git clone https://github.com/hakimel/reveal.js.git
+            fi
+
+            cd reveal.js
+            if [ ! -d node_modules ]; then
+              echo "Installing reveal.js dependencies..."
+              npm install
+            fi
+          '';          
         };
+
       });
     };
 }
