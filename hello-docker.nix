fe42338ba9ca8e0b47f7ea0075065704c8336843
Jakob Guldberg Aaes <jakob.aaes@res-group.com>
2024-08-21 20:38:01 +0200

    added final touches

---
+++ hello-docker.nix
@@ -0,0 +1,12 @@
+# hello-docker.nix
+
+{ pkgs ? import <nixpkgs> { }
+, pkgsLinux ? import <nixpkgs> { system = "x86_64-linux"; }
+}:
+
+pkgs.dockerTools.buildImage {
+  name = "hello-docker";
+  config = {
+    Cmd = [ "${pkgsLinux.hello}/bin/hello" ];
+  };
+}
